41b12f2ef64affd7fd0dad2e981291cd14f2e2bc
EternalDuelist <davcarranza@gmail.com>
2015-01-10 11:39:20 -0800

    some name magic

--- script1.js
+++ script1.js
@@ -7,8 +7,8 @@ function Quarter(id){
 	this.id = id;
 	this.html = myhtml;
 	this.extra = 'ondrop="drop(event)" ondragover="allowDrop(event)"';
-	this.style = 'width:400px;\
-		height:400px;\
+	this.style = 'width:250px;\
+		height:175px;\
 		padding:10px;\
 		border:1px solid #aaaaaa;\
 		overflow-y: auto;\
@@ -19,18 +19,24 @@ function Quarter(id){
 
 function myhtml(){
 	return '<div id ="' + this.id + '" ' + this.extra + 'style ="' + this.style + '"></div>';
-}/*
-function quarterEnum(){
-	ver temp == new Object();
-	temp['0'] = 'FALL';
-	temp['1'] = 'WINTER';
-	temp['2'] = 'SPRING';
-	temp['3'] = 'SUMMER';
-	return temp;
-}*/
+}
+
+function quarterEnum(num){
+	switch(num){
+		case 0: return 'FALL';
+			break;
+		case 1: return 'WINTER';
+			break;
+		case 2: return 'SPRING';
+			break;
+		case 3: return 'SUMMER';
+			break;
+	}
+}
+
 $(document).ready(function(){
   $("#update").click(function(){
-	var divOne = new Quarter('lol');
+	var divOne = new Quarter(quarterEnum(2) + 2012);
 	document.getElementById('schedule').innerHTML = divOne.html();
 	$("#schedule").show();
   });

0f1084b7e49925ef070f6b67b63a0b0d071b98c9
Justin Irvin Andaya Barros <jbarros@unix2.lt.ucsc.edu>
2015-01-10 00:48:14 -0800

    updated course.js file

--- course.js
+++ course.js
@@ -1,19 +1,67 @@
-function format_name (p) {
+function parseName (p) {
+   if (p != null) {
+      var token;
+      for (var i = 0; i < p.length; i++) {
+         if (p.charAt(i) == ":") break;
+         else token = p.substring(0, i+1);
+      }
+      return token;
+   }
+   else console.log("Invalid String: " + p);
 }
 
-function format_title (p) {
+function parseTitle (p) {
+   if (p != null) {
+      var token;
+      for (var i = 0; i < p.length; i++) {
+         if (p.charAt(i) == ":") token = p.substring(i+2, p.length);
+      }
+      return token;
+   }
+   else console.log("Invalid String: " + p);
 }
 
-function format_id (p) {
+function parseId (p) {
+   if (p != null) {
+      var token, m = "", n;
+      for (var i = 0; i < p.length; i++) {
+         if (p.charAt(i) != ":") {
+            if (!isNaN(p.charAt(i))) {
+               m += p.charAt(i);
+               if (p.charAt(i+1) == ";") n = "";
+               else n = p.charAt(i+1);
+            }
+         }
+         else break;
+      }
+      m = parseInt(m);
+      token = m + "" + n;
+      return token;
+   }
 }
 
-function format_department (p) {
+function parseDepartment (p) {
+   if (p != null) {
+      var token;
+      for (var i = 0; i < p.length; i++) {
+         if (isNaN(p.charAt(i))) token = p.substring(0, i+1);
+         else break;
+      }
+      return token;
+   }
 }
 
-function format_reqs (p) {
+function parseReqs (p) {
 }
 
 function Course (p) {
+   this.properties = p.toString();
+   this.name = parseName(p).toString();
+   this.title = parseTitle(p).toString();
+   this.id = parseId(p).toString();
+   this.department = parseDepartment(p).toString();
+   this.pre_reqs = [];
+   this.units = 5;
 }
 
 function printCourse (c) {

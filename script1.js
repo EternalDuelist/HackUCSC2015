417fded89807f5e1519c64792adb472d640677ac
EternalDuelist <davcarranza@gmail.com>
2015-01-09 21:04:53 -0800

    updates

---
+++ script1.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $("#update").click(function(){
+    $("#schedule").hide();
+	document.getElementById('schedule').innerHTML = "lol";
+	$("#schedule").show();
+  });
+});

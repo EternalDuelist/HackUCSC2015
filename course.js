d6da1b802d634fa0f455c67a4dc94c8957eabd98
Justin Irvin Andaya Barros <jbarros@unix2.lt.ucsc.edu>
2015-01-10 00:33:45 -0800

    created course.js file

---
+++ course.js
@@ -0,0 +1,26 @@
+function format_name (p) {
+}
+
+function format_title (p) {
+}
+
+function format_id (p) {
+}
+
+function format_department (p) {
+}
+
+function format_reqs (p) {
+}
+
+function Course (p) {
+}
+
+function printCourse (c) {
+}
+
+function readfile (f1, f2, f3) {
+}
+
+function main () {
+}

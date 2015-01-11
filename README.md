7260b62ea51498a28fb70bcab1ae43d9fea2e8cc
EternalDuelist <davcarranza@gmail.com>
2015-01-11 05:13:20 -0800

    Sexyness intensifies

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ and implement some of the functionality of the site
 
 Tutorial to help with implementing hashes:
 http://www.mojavelinux.com/articles/javascript_hashes.html
+
+This tutorial was used as a reference to implement the drag and drop in our schedule:
+http://www.w3schools.com/html/html5_draganddrop.asp
+
+Tutorial used as a reference to make the navigational bar:
+http://www.w3schools.com/website/web_spa_navigation.asp
--- script1.js
+++ script1.js
@@ -20,10 +20,23 @@ function update(){
 }
 
 */
+function allowDrop(event) {
+    event.preventDefault();
+}
 
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    var dataT = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(dataT));
+}
 function Quarter(id){
 	this.id = id;
 	this.html = myhtml;
+	this.nestedTags  = "";
 	this.extra = 'ondrop="drop(event)" ondragover="allowDrop(event)"';
 	this.style = 'width:250px;\
 		height:175px;\
@@ -38,7 +51,7 @@ function Quarter(id){
 
 function myhtml(){
 	return  '<div id ="' + this.id + '" ' + this.extra +
-			'style ="' + this.style + '">'+this.id+'<hr><br></div>';
+			'style ="' + this.style + '">'+this.id+'<hr><br>' + this.nestedTags + '</div>';
 }
 
 function quarterEnum(num){
@@ -70,11 +83,15 @@ $(document).ready(function(){
 		var startYear = ((inputTest(document.getElementById('syear')))? parseInt(document.getElementById('syear').value) : 2015);
 		var numYear   = ((inputTest(document.getElementById('nyear')))? parseInt(document.getElementById('nyear').value) : 4);
 		var summer    = ((inputTest(document.getElementById('sumer')))? parseInt(document.getElementById('sumer').value) : 0);
+		
+		var clss = 1;
 		for (i = 0; i < numYear; i++){
 			for(j = 0; j < (3+summer); j++){
 				var year = startYear + i + ((j ==0)? 0 : 1);
 				var key = quarterEnum(j)+year;
 				hQuarts[key] = new Quarter(key);
+				clss++;
+				hQuarts[key].nestedTags = '<img id="gClass'+ clss +'" src="images/genClass.bmp" draggable="true" ondragstart="drag(event)" width="200" height="50">';
 				document.getElementById('schedule').innerHTML += hQuarts[key].html();
 			}
 			document.getElementById('schedule').innerHTML += '<hr><br>';

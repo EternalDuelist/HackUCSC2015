0abedde776a82ec1e839cbe76f86d17cecf3f5ae
Justin Irvin Andaya Barros <jbarros@unix2.lt.ucsc.edu>
2015-01-10 22:07:13 -0800

    created graph.js file

---
+++ graph.js
@@ -0,0 +1,62 @@
+function Graph(name, value) {
+   this.name = name;
+   this.value = value;
+   this.quarter = 0;
+   this.prereq = new Object();
+}
+
+function Quarter(name, capacity) {
+   this.name = name;
+   this.capacity = capacity;
+   this.courses = 0;
+   this.courseList = [];
+}
+
+function hashData(hash, name, data) {
+   hash[name] = data;
+}
+
+function printHash(prefix,hash) {
+   for (var node in hash) {
+      if (hash.hasOwnProperty(node)) {
+         console.log(prefix + 'key is: ' + hash[node].name + ', Quarter is: ' + hash[node].quarter + '<br>');
+      }
+   }
+}
+
+function printGraph(graph) {
+   console.log('<br>' + graph.name + ' has prerequisites:<br>');
+   printHash('> ', graph.prereq)
+}
+
+function printHashPR(hash) {
+   for (var node in hash) {
+      if (hash.hasOwnPropert(node)) {
+         printGraph(hash[node]);
+      }
+   }
+}
+
+function insertClass(graph, schedule) {
+   if (graph.quarter < schedule.length) {
+      schedule[graph.quarter].courseList.push(graph);
+   }
+}
+
+function dSort(graph, schedule) {
+   for (var node in graph.prereq) {
+      if (graph.prereq.hasOwnProperty(node)) {
+         var temp = dSort(graph.prereq[node], schedule)'
+         if (temp > graph.quarter) {
+            graph.quarter = temp;
+         }
+      }
+   }
+
+   if (graph.value == 0) {
+      console.log(graph.name + " ");
+      inserClass(graph, schedule);
+      graph.value = 1;
+      return graph.quarter + 1;
+   }
+}
